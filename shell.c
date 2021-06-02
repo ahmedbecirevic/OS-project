633dbf8e35658f3dbd62b25828af4298d16188c6
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2021-06-02 20:08:42 +0200

    add line reading

--- shell.c
+++ shell.c
@@ -1,13 +1,71 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include<unistd.h>
+#include <unistd.h>
 
 #define TRUE 1
 
-int main () {
+char *line_read (void) {
+   char *line = NULL;
+   ssize_t sizeOfBuff = 0;
+
+   if (getline(&line, &sizeOfBuff, stdin) == -1) {
+      // check if end of file -> EOF
+      if (feof(stdin)) {
+         exit(EXIT_SUCCESS);
+      } else {
+         perror("lineReading");
+         exit(EXIT_FAILURE);
+      }
+   }
+   return line;
+}
+
+char **line_split (char *line) {
+   int pos = 0;
+   char **tokens = malloc(64 * sizeof(char*));
+   char *token;
+
+   if (!tokens) {
+      fprintf(stderr, "error when allocating\n");
+      exit(EXIT_FAILURE);
+   }
+
+   token = strtok(line, LSH_TOK_DELIM);
+   while (token != NULL) {
+      tokens[pos] = token;
+      pos++;
+
+      token = strtok(NULL, LSH_TOK_DELIM);
+   }
+   tokens[pos] = NULL;
+   return tokens;
+}
+
+void commands_input_loop (void) {
+   char **arguments;
+   char *line;
+   int state;
+
+// use do while loop beacuse it executes once before it checks its value 
+   do (state) {
+      printf("prompt$ ");
+      // read user input command
+      line = line_read();
+      arguments = line_split(line);
+      state = line_execute(arguments);
+
+      free(line);
+      free(arguments);
+   } while (state);
+}
+
+
+int main (int argc, char **argv) {
    int pid, i = 0;
    char tab[256], *s;
+
+   commands_input_loop();
 
    while(TRUE) {
       printf("prompt %d", i);
